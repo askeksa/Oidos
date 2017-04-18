e023d2164a1fa5bb08cddbccd0a9732efe234021
mathieu _alkama_ m <code@alkama.com>
2017-04-18 21:06:56 +0200

    Fixes build for MacOS

--- synth/build.rs
+++ synth/build.rs
@@ -1,6 +1,11 @@
 extern crate nasm_rs;
 
 fn main() {
+	#[cfg(not(target_env = "msvc"))]
+	nasm_rs::compile_library("libadditive.a", &["src/additive.asm"]);
+
+	#[cfg(target_env = "msvc")]
 	nasm_rs::compile_library("additive.lib", &["src/additive.asm"]);
+
 	println!("cargo:rustc-link-lib=static=additive");
 }
